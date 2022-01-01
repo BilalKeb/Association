305106fafb8352385bd670a7093ed1f22302e2df
BilalKeb <90609831+BilalKeb@users.noreply.github.com>
2022-01-01 15:31:34 +0100

    Association

--- Routes/association.js
+++ Routes/association.js
@@ -1,19 +1,31 @@
-
-
 const express = require("express")
 const app = express()
 
 
 // je recupere mon ficjier Json
-let associations = require("../association.json")
+const associations = require("../association.json")
 
+
+const checkingAssociation = (req , res , next) => {
+    const { name } = req.params
+
+    const association = associations.find (element => element.name === name)
+        
+        if (association) {
+            next()
+        } else {
+            res.status(404).send("Can Not get association")
+        }
+     }
 
 app.get ('/association' ,(req , res) => {
  res.json(associations)   
 })
 
-app.get('/association/:name', (req , res) =>{
-    const  {name} = req.params
-    const nameAssociation = nameAssociations.find(nameAssociation.name === name)
+app.get('/association:name', checkingAssociation,(req , res) =>{
+    const  {name, image , slogan, decription} = req.params
+    const nameAssociation = nameAssociations.find(nameAssociation.name === name , image , slogan, decription)
     res.json(nameAssociation)
+
 })
+
